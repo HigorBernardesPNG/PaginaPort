7c16c6e8dee86ed5974b12090783b3e2621622ae
Higor Bernardes <zdtshark@gmail.com>
2024-12-09 10:10:45 -0400

    atualizando CSS

--- BibliotecaJS/funcaoImportPag.js
+++ BibliotecaJS/funcaoImportPag.js
@@ -23,5 +23,5 @@ function importarHTML(caminhoDoArquivo, idElementoDestino) {
 
 // - Chamando a função -
 
-importarHTML('../HtmlExport/header.html', 'secaoHeader');
+importarHTML('/HtmlExport/header.html', 'secaoHeader');
 
